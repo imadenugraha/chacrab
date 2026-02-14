6a21bd60cd24fc321728e57ebde16b118486006e
imadenugraha <imadenugraha@gmail.com>
2026-02-14 21:14:58 +0700

    refactor: change queries module visibility to crate level

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -4,4 +4,4 @@ pub mod queries;
 
 pub use db::{init_db, Database};
 pub use keyring::{delete_session_key, get_session_key, has_session_key, save_session_key};
-pub use queries::*;
+pub(crate) use queries::*;
